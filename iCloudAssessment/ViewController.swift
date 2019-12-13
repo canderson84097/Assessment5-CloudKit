082e9960922a207b08818477a97e1ec9e94cefbf
Chris Anderson <cda84097@gmail.com>
2019-12-13 09:00:30 -0700

    Initial Commit

---
+++ iCloudAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCloudAssessment
+//
+//  Created by Chris Anderson on 12/13/19.
+//  Copyright © 2019 Renaissance Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
